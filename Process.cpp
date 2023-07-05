843e0c301bf8e17513409a5c0f0292d7cbfb46e0
mgreig19 <128261489+mgreig19@users.noreply.github.com>
2023-07-05 17:34:24 -0500

    Update Process.cpp
    added comments

--- Process.cpp
+++ Process.cpp
@@ -7,27 +7,39 @@
 #include <iterator>
 #include <map>
 #include <string>
+#include <utility>
 
 Using namespace std;
 
+/**********************
+Default Constructor
+**********************/
 dataProcessing::dataProcessing()
 {
 }
 
+/*************************
+Constructor
+*************************/
 dataProcessing::dataProcessing(list<int> inputList, int compVal, string inputString, int shiftVal)
 {
 	setInputList(list<int> inputList);
-	setInputString(inputString);
+	setInputString(string inputString);
 	setShiftVal(shiftVal);
 	setCompVal(compVal);
 }
 
+/**************************
+Function: aboveBelow
+Use: compares an input value to a list of numbers and records how many numbers in the list are above or below the input comparison value
+Args: list<int>, int
+***************************/
 Map<string, int> dataProcessing::aboveBelow(list<int> inputList, int compVal)
 {
 	int above = 0;
 	int below = 0;
-	for(I=0, I < inputList.size(), I++){
-		if (inputList[I] < compVal){
+	for(i=0, i < inputList.size(), i++){
+		if (inputList[i] < compVal){
 			below++;
 		}
 		else if (inputList[I] > compVal){
@@ -39,12 +51,20 @@ Map<string, int> dataProcessing::aboveBelow(list<int> inputList, int compVal)
 	return mapResponse;
 }
 
+/*************************
+Funtion: stringRotation
+Use: rotates the characters in the passed in sting to the right by a given value
+Args: string, int
+*************************/
 String dataProcessing::stringRotation(string inputString, int shiftVal)
 {
 	shiftString = leftrotate(inputString, inputString.length()-shiftVal);
 	return shiftString;	
 }
 
+/***********************************
+Setter Methods for Class Contruction
+************************************/
 Void dataProcessing::setInputList(list<int> newList)
 {
 	inputList = newList;
